1a29145b1bf7547f70c3dd644e7b23ead3ae54b3
ckhe1215 <ckhe1215@gmail.com>
2023-09-11 02:02:38 +0900

    :sparkles: Feat: Upcoming stock 카드 추가

--- src/components/home/UpcomingStockSection.tsx
+++ src/components/home/UpcomingStockSection.tsx
@@ -4,9 +4,77 @@ import { styled } from "styled-components";
 import { getFonts } from "@/styles/fonts";
 import ScopeBar from "../common/ScopeBar";
 import { useState } from "react";
+import UpcomingStock from "./UpcomingStock";
+
+type Subscription = "disable" | "able" | "limit";
+
+interface UpcomingStockDataType {
+  id: string;
+  title: string;
+  love: boolean;
+  category: string;
+  account: string;
+  minPrice: number;
+  maxPrice: number;
+  subscription?: Subscription;
+  subscriptionDueDate: string;
+  accountDueDate: string;
+}
 
 const UpcomingStockSection = () => {
   const [isShowingAllStocks, setIsShowingAllStocks] = useState(false);
+  const upcomingStockData: UpcomingStockDataType[] = [
+    {
+      id: "0",
+      title: "에이치엠씨아이비스팩6호",
+      love: true,
+      category: "의료장비 및 서비스",
+      account: "대신증권",
+      minPrice: 26000,
+      maxPrice: 31000,
+      subscription: "limit",
+      subscriptionDueDate: "8월 25일",
+      accountDueDate: "8월 25일",
+    },
+    {
+      id: "1",
+      title: "에이엘티",
+      love: false,
+      category: "자본재",
+      account: "대신증권",
+      minPrice: 26000,
+      maxPrice: 31000,
+      subscriptionDueDate: "8월 26일",
+      accountDueDate: "8월 25일",
+    },
+    {
+      id: "2",
+      title: "시지트로닉스",
+      love: true,
+      category: "내구 소비재 및 의류",
+      account: "시지트로닉스",
+      minPrice: 26000,
+      maxPrice: 31000,
+      subscription: "able",
+      subscriptionDueDate: "8월 31일",
+      accountDueDate: "8월 16일",
+    },
+    {
+      id: "3",
+      title: "에이엘티",
+      love: true,
+      category: "내구 소비재 및 의류",
+      account: "대신증권",
+      minPrice: 26000,
+      maxPrice: 31000,
+      subscription: "disable",
+      subscriptionDueDate: "9월 1일",
+      accountDueDate: "8월 25일",
+    },
+  ];
+  const filteredUpcomingStockData = isShowingAllStocks
+    ? upcomingStockData
+    : upcomingStockData.filter((data) => data.subscription !== "disable");
 
   return (
     <section>
@@ -19,7 +87,22 @@ const UpcomingStockSection = () => {
         allStocksCount={5}
         availableStocksCount={4}
       />
-      <UpcomingStockList>card</UpcomingStockList>
+      <UpcomingStockList>
+        {filteredUpcomingStockData.map((data) => (
+          <UpcomingStockItem key={data.id}>
+            <UpcomingStock.status status={true}>{data.subscriptionDueDate}</UpcomingStock.status>
+            <UpcomingStock.cardWrap
+              title={data.title}
+              love={data.love}
+              category={data.category}
+              account={data.account}
+              price={[data.minPrice, data.maxPrice]}
+              subscription={data.subscription}
+              date={data.subscriptionDueDate}
+            />
+          </UpcomingStockItem>
+        ))}
+      </UpcomingStockList>
     </section>
   );
 };
@@ -39,4 +122,13 @@ const TitleWrapper = styled.div`
 const UpcomingStockList = styled.div`
   background: linear-gradient(180deg, #f7f8f9 0%, #f7f8f9 93.35%, rgba(247, 248, 249, 0) 100%);
   padding-bottom: 80px;
+  padding-inline: 16px;
+`;
+
+const UpcomingStockItem = styled.div`
+  margin-bottom: 32px;
+
+  &:first-child {
+    padding-top: 20px;
+  }
 `;
